c8662836885ade52e3eeb581de3b049888635ee1
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 <jade@jadecharles.com>
2023-04-01 11:10:38 -0700

    Dist added

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "IrisTransition", {
+  enumerable: true,
+  get: function get() {
+    return _irisTransition.default;
+  }
+});
+var _irisTransition = _interopRequireDefault(require("./navigation/iris-transition"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
---
+++ dist/navigation/iris-transition.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var IrisTransition = function IrisTransition(_ref) {
+  var children = _ref.children;
+  return /*#__PURE__*/_react.default.createElement("div", null, "Iron Transition v0.1.0");
+};
+var _default = IrisTransition;
+exports.default = _default;
